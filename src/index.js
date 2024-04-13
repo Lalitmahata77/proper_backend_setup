2a8b0218ff974b0d72f9f7006877d22c8e747498
Lalit-mahata <lm3654725@gmail.com>
2024-04-14 00:14:23 +0545

    all setup congigured

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv"
+import { dbConnect } from "./db/dbConnect.js"
+import app from "./app.js";
+dotenv.config({
+    path: "./env"
+})
+const Port = process.env.PORT || 4000
+dbConnect().then(()=>{
+    console.log("DB connected");
+}).catch((err)=>{
+    console.log("error",err);
+})
+
+app.listen(Port,()=>{
+    console.log(`server is listening on port :${Port}`);
+})
